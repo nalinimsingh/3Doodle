a48b3348af18980c8cded5a7f4013b2718e77663
Nalini Singh <nmsingh@mit.edu>
2017-07-29 21:11:24 -0700

    Plot average point and re-detect if all features drop out

--- cv_testing/klt_tracking.py
+++ cv_testing/klt_tracking.py
@@ -11,31 +11,28 @@ def mask_image(orig_image):
 
 def main():
   dir = os.path.dirname(__file__)
-  path = os.path.join(dir,'../mock/green_pen_manual/trial2')
+  path = os.path.join(dir,'../mock/green_pen_manual/trial1')
   images = [os.path.join(path,f) for f in os.listdir(path) if not f.startswith('.')]
 
   feature_params = dict( maxCorners = 100,
                          qualityLevel = 0.00001,
-                         minDistance = 3,
+                         minDistance = 5,
                          blockSize = 7 )
 
-  lk_params = dict( winSize  = (3,3),
+  lk_params = dict( winSize  = (5,5),
                     maxLevel = 10,
-                    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 150, 0.03))
+                    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 100, 0.05))
 
   # Find initial features
   old_frame = np.asarray(cv2.imread(images[0]))
   old_masked = mask_image(old_frame)
   old_gray = cv2.cvtColor(old_masked, cv2.COLOR_BGR2GRAY)
 
+  regenerate_features = False
   # Find initial features
   p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
-  # Plot features
-  markers = np.zeros_like(old_gray)
-  for point in p0:
-    print point
-    cv2.circle(old_masked, tuple(point[0]), 5, (0,0,255), 2)  
+  point_history = []
 
   for img in images[1:]:
       frame = np.asarray(cv2.imread(img))
@@ -44,6 +41,8 @@ def main():
 
       # Calculate optical flow
       p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+      if p1 is None:
+        p1 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
       # Select good points
       good_new = p1[st==1]
@@ -51,17 +50,24 @@ def main():
       # Plot features
       markers = np.zeros_like(frame_gray)
       for point in good_new:
-          cv2.circle(frame_masked, tuple(point), 5, (0,0,255), 2)
+          cv2.circle(frame, tuple(point), 5, (0,0,255), 2)
+
+      # Plot average
+      avg = np.average(good_new,axis=0)
+      point_history.append(avg)
+      cv2.circle(frame_masked, tuple(avg), 5, (255,255,0), 2)
 
       # Show current frame    
-      cv2.imshow('frame',cv2.resize(frame_masked, (0,0), fx=0.5, fy=0.5))
+      cv2.imshow('frame',cv2.resize(frame, (0,0), fx=0.5, fy=0.5))
       time.sleep(0.5)
       k = cv2.waitKey(30) & 0xff
       if k == 27:
           break
 
       old_gray = frame_gray.copy()
+
       p0 = good_new.reshape(-1,1,2)
+
 
   cv2.destroyAllWindows()
 
