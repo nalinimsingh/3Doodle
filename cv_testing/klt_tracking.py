8af13954d40ba4dc865eaa1545cb11370c9c0e18
Nalini Singh <nmsingh@mit.edu>
2017-07-29 18:25:58 -0700

    Clean up code

--- cv_testing/klt_tracking.py
+++ cv_testing/klt_tracking.py
@@ -1,66 +1,69 @@
 import sys
 import os
 import time
-
-sys.path.append("/usr/local/lib/python2.7/site-packages/") 
-
 import numpy as np
 import cv2
 
-dir = os.path.dirname(__file__)
-path = os.path.join(dir,'../mock/green_pen_manual/trial2')
-images = [os.path.join(path,f) for f in os.listdir(path) if not f.startswith('.')]
+def mask_image(orig_image):
+  mask = np.all([orig_image[:,:,0]<100,orig_image[:,:,1]>100,orig_image[:,:,2]<100],axis=0)
+  masked_image = orig_image*mask[:,:,np.newaxis]
+  return masked_image
 
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.00001,
-                       minDistance = 3,
-                       blockSize = 7 )
+def main():
+  dir = os.path.dirname(__file__)
+  path = os.path.join(dir,'../mock/green_pen_manual/trial2')
+  images = [os.path.join(path,f) for f in os.listdir(path) if not f.startswith('.')]
 
-lk_params = dict( winSize  = (3,3),
-                  maxLevel = 10,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 150, 0.03))
+  feature_params = dict( maxCorners = 100,
+                         qualityLevel = 0.00001,
+                         minDistance = 3,
+                         blockSize = 7 )
 
-# Find initial features
-old_frame = np.asarray(cv2.imread(images[0]))
-mask = np.all([old_frame[:,:,0]<100,old_frame[:,:,1]>100,old_frame[:,:,2]<100],axis=0)
-old_masked = old_frame*mask[:,:,np.newaxis] 
-old_gray = cv2.cvtColor(old_masked, cv2.COLOR_BGR2GRAY)
+  lk_params = dict( winSize  = (3,3),
+                    maxLevel = 10,
+                    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 150, 0.03))
 
-# Find initial features
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+  # Find initial features
+  old_frame = np.asarray(cv2.imread(images[0]))
+  old_masked = mask_image(old_frame)
+  old_gray = cv2.cvtColor(old_masked, cv2.COLOR_BGR2GRAY)
 
-# Plot features
-markers = np.zeros_like(old_gray)
-for point in p0:
-  print point
-  cv2.circle(old_masked, tuple(point[0]), 5, (0,0,255), 2)  
+  # Find initial features
+  p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
-for img in images[1:]:
-    frame = np.asarray(cv2.imread(img))
-    mask = np.all([frame[:,:,0]<100,frame[:,:,1]>100,frame[:,:,2]<100],axis=0)
-    frame_masked = frame*mask[:,:,np.newaxis]
-    frame_gray = cv2.cvtColor(frame_masked, cv2.COLOR_BGR2GRAY)
+  # Plot features
+  markers = np.zeros_like(old_gray)
+  for point in p0:
+    print point
+    cv2.circle(old_masked, tuple(point[0]), 5, (0,0,255), 2)  
 
-    # Calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+  for img in images[1:]:
+      frame = np.asarray(cv2.imread(img))
+      frame_masked = mask_image(frame)
+      frame_gray = cv2.cvtColor(frame_masked, cv2.COLOR_BGR2GRAY)
 
-    # Select good points
-    good_new = p1[st==1]
+      # Calculate optical flow
+      p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-    # Plot features
-    markers = np.zeros_like(frame_gray)
-    for point in good_new:
-        cv2.circle(frame_masked, tuple(point), 5, (0,0,255), 2)
+      # Select good points
+      good_new = p1[st==1]
 
-    # Show current frame    
-    cv2.imshow('frame',cv2.resize(frame_masked, (0,0), fx=0.5, fy=0.5))
-    time.sleep(0.5)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+      # Plot features
+      markers = np.zeros_like(frame_gray)
+      for point in good_new:
+          cv2.circle(frame_masked, tuple(point), 5, (0,0,255), 2)
 
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
+      # Show current frame    
+      cv2.imshow('frame',cv2.resize(frame_masked, (0,0), fx=0.5, fy=0.5))
+      time.sleep(0.5)
+      k = cv2.waitKey(30) & 0xff
+      if k == 27:
+          break
 
-cv2.destroyAllWindows()
+      old_gray = frame_gray.copy()
+      p0 = good_new.reshape(-1,1,2)
 
+  cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
