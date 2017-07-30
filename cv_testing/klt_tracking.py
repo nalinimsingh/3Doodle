48f9643cc5c5401c9f2f09bcb5c89813d0f932b1
Nalini Singh <nmsingh@mit.edu>
2017-07-30 00:53:00 -0700

    Create KLTTracker class

--- cv_testing/klt_tracking.py
+++ cv_testing/klt_tracking.py
@@ -6,81 +6,103 @@ import scipy.cluster
 import cv2
 import itertools
 
-def mask_image(orig_image):
-  mask = np.all([orig_image[:,:,0]<100,orig_image[:,:,1]>100,orig_image[:,:,2]<100],axis=0)
-  masked_image = orig_image*mask[:,:,np.newaxis]
-  return masked_image
+def mask_image(image):
+    mask = np.all([image[:,:,0]<100,image[:,:,1]>100,image[:,:,2]<100],axis=0)
+    masked_image = image*mask[:,:,np.newaxis]
+    return masked_image
 
-def main():
-  dir = os.path.dirname(__file__)
-  path = os.path.join(dir,'../mock/green_pen_manual/trial1')
-  images = [os.path.join(path,f) for f in os.listdir(path) if not f.startswith('.')]
-  xmin = 100
-  xmax = 1650
-  ymin = 150
-  ymax = 1000
-
-  feature_params = dict( maxCorners = 10,
+class KLTTracker():
+    def __init__(self):
+        self.xmin = 100
+        self.xmax = 1650
+        self.ymin = 150
+        self.ymax = 1000
+        self.feature_params = dict(maxCorners = 10,
                          qualityLevel = 0.00001,
                          minDistance = 5,
                          blockSize = 7 )
 
-  lk_params = dict( winSize  = (5,5),
+        self.lk_params = dict( winSize  = (5,5),
                     maxLevel = 10,
                     criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 100, 0.05))
 
-  # Find initial features
-  old_frame = np.asarray(cv2.imread(images[0]))[ymin:ymax,xmin:xmax,:]
-  old_masked = mask_image(old_frame)
-  old_gray = cv2.cvtColor(old_masked, cv2.COLOR_BGR2GRAY)
+    # initial_img_array: Numpy array
+    def detect_features(self,initial_img_array):
+        old_frame = initial_img_array[self.ymin:self.ymax,self.xmin:self.xmax,:]
+        old_masked = mask_image(old_frame)
+        self.old_gray = cv2.cvtColor(old_masked, cv2.COLOR_BGR2GRAY)
+        self.p0 = cv2.goodFeaturesToTrack(self.old_gray, mask = None, **self.feature_params)
 
-  regenerate_features = False
-  # Find initial features
-  p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    def init_feature_tracking(self):
+        self.point_history = []
 
-  point_history = []
+    def track_features(self,input_img_array):
+        self.frame = input_img_array[self.ymin:self.ymax,self.xmin:self.xmax,:]
+        frame_masked = mask_image(self.frame)
+        self.frame_gray = cv2.cvtColor(frame_masked, cv2.COLOR_BGR2GRAY)
 
-  for img in images[1:]:
-      frame = np.asarray(cv2.imread(img))[ymin:ymax,xmin:xmax,:]
-      frame_masked = mask_image(frame)
-      frame_gray = cv2.cvtColor(frame_masked, cv2.COLOR_BGR2GRAY)
+        # Calculate optical flow
+        self.p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_gray, self.frame_gray, self.p0, None, **self.lk_params)
 
-      # Calculate optical flow
-      p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+        # Select good points
+        self.good_new = self.p1[st==1]
 
-      # Select good points
-      good_new = p1[st==1]
-      if len(good_new)==0:
-        break
+    def cluster(self):
+      clusters= scipy.cluster.hierarchy.fclusterdata(self.good_new, 1, criterion='inconsistent', metric='euclidean', depth=2, method='average', R=None)
+      self.pen_tip = self.good_new[clusters == 1]
 
-      # Cluster to find the biggest collection of points
-      clusters= scipy.cluster.hierarchy.fclusterdata(good_new, 1, criterion='inconsistent', metric='euclidean', depth=2, method='average', R=None)
-      pen_tip = good_new[clusters == 1]
+    def average(self):
+        self.avg = np.average(self.pen_tip,axis=0)
+        self.point_history.append(self.avg)
 
-      # Plot features
-      markers = np.zeros_like(frame_gray)
-      for point in pen_tip:
-        cv2.circle(frame, tuple(point), 5, (0,0,255), 2)
+        for a,b in itertools.izip(self.point_history, self.point_history[1:]):
+            cv2.line(self.frame, tuple(a), tuple(b), (255,0,0))
 
-      # Plot average
-      avg = np.average(pen_tip,axis=0)
-      point_history.append(avg)
+    # Prepare for the next round of tracking
+    def update(self):
+        self.old_gray = self.frame_gray.copy()
+        self.p0 = self.good_new.reshape(-1,1,2)
 
-      for a,b in itertools.izip(point_history, point_history[1:]):
-        cv2.line(frame, tuple(a), tuple(b), (255,0,0))
+    def get_frame(self):
+        return self.frame
 
-      # Show current frame    
-      cv2.imshow('frame',cv2.resize(frame, (0,0), fx=0.5, fy=0.5))
-      time.sleep(0.1)
-      k = cv2.waitKey(30) & 0xff
-      if k == 27:
-          break
+    def get_frame_gray(self):
+        return self.frame_gray
 
-      old_gray = frame_gray.copy()
+    def get_good_new(self):
+        return self.good_new
 
-      p0 = good_new.reshape(-1,1,2)
+    def get_pen_tip(self):
+        return self.pen_tip
 
-  cv2.destroyAllWindows()
+
+def main():
+    dir = os.path.dirname(__file__)
+    path = os.path.join(dir,'../mock/green_pen_manual/trial1')
+    images = [os.path.join(path,f) for f in os.listdir(path) if not f.startswith('.')]
+
+    feature_tracker = KLTTracker()
+    feature_tracker.detect_features(np.asarray(cv2.imread(images[0])))
+    feature_tracker.init_feature_tracking()
+
+    for img in images[1:]:
+        feature_tracker.track_features(np.asarray(cv2.imread(img)))
+        if len(feature_tracker.get_good_new()) == 0:
+            break
+        feature_tracker.cluster()
+        feature_tracker.average()
+        feature_tracker.update()
+
+        markers = np.zeros_like(feature_tracker.get_frame_gray())
+        for point in feature_tracker.get_pen_tip():
+            cv2.circle(feature_tracker.get_frame(), tuple(point), 5, (0,0,255), 2)
+
+        cv2.imshow('frame',cv2.resize(feature_tracker.get_frame(), (0,0), fx=0.5, fy=0.5))
+        time.sleep(0.1)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+
 
 if __name__ == "__main__":
     main()
