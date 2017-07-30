6c6935097609d4d47ee49e64ee13d1c17ac4d73e
Zareen Choudhury <zareenc@users.noreply.github.com>
2017-07-29 20:31:47 -0700

    Testing web socket

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,2 +1,36 @@
+// Setup data socket
+// var socket = new io.Socket('localhost',{
+// 	port: 8080
+// });
+
+// socket.on('connect',function() {
+// 	console.log('Client has connected to the server!');
+// });
+
+// socket.on('message',function(data) {
+// 	console.log('Received a message from the server!', data);
+// });
+
+// socket.on('disconnect',function() {
+// 	console.log('The client has disconnected!');
+// });
+
+// socket.connect();
+
+
+var socket = new WebSocket('ws://localhost:8080');
+socket.onopen = function(event) {
+	console.log('Socket open');
+
+	socket.onmessage = function(event) {
+		console.log('Client received a message', event);
+	};
+	
+	socket.onclose = function(event) {
+		console.log('Socket closed');
+	};
+}
+
+// Drawing initialization
 init();
 testDrawLine();
